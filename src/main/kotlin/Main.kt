705d50cde6b5cdb1e7b698d5fe612e46c9cfb0eb
rnk0085 <rinawkmbi4k8@gmail.com>
2023-05-21 17:20:23 +0900

    feat: 値オブジェクトは値を変更するふるまいを持たない

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,12 @@
+import domain.BadUserName
 import domain.User
 import domain.UserId
 import domain.UserName
 
 fun main() {
-    check1()
+    check2()
+
+    check3()
 }
 
 /**
@@ -22,4 +25,26 @@ private fun check1() {
 
     println(user1) // OK
     println(user2) // エラー：Exception in thread "main" java.lang.IllegalArgumentException: ユーザー名は5文字以上です
+}
+
+/**
+ * UserName が自身の値を変更する？
+ */
+private fun check2() {
+    val rnk0085 = UserName("rnk0085")
+    println("rnk0085 = $rnk0085")
+
+    val takenoko = rnk0085.changeUserName("takenoko")
+    println("takenoko = $takenoko")
+}
+
+/**
+ * 自分自身の値を変えると気持ちの悪いことに…
+ */
+private fun check3() {
+    val rnk0085 = BadUserName("rnk0085")
+    println("rnk0085 = $rnk0085")
+
+    rnk0085.changeUserName("takenoko")
+    println("rnk0085 = $rnk0085")
 }
--- src/main/kotlin/domain/UserName.kt
+++ src/main/kotlin/domain/UserName.kt
@@ -3,6 +3,8 @@ package domain
 /**
  * ルールが存在している
  * UserName は「5文字以上」であることが保証される
+ *
+ * Memo: Value class primary constructor must only have final read-only (val) property parameters
  */
 @JvmInline
 value class UserName(
@@ -10,5 +12,32 @@ value class UserName(
 ) {
     init {
         require(name.length >= 5) { "ユーザー名は5文字以上です" }
+    }
+
+    // TODO: 値オブジェクトなので自身を変更するふるまいはNG？
+    // これって「代入」にあたるかも？
+    fun changeUserName(other: String) : UserName {
+        return UserName(other)
+    }
+
+//    fun changeUserName2(other: String) {
+//        // FIXME: Val cannot be reassigned
+//        this.name = other
+//    }
+}
+
+/**
+ * FIXME: 自分自身の値を変更するふるまいは持つべきではない
+ */
+data class BadUserName(
+    // FIXME: 不変の方が良い
+    private var name: String,
+) {
+    init {
+        require(name.length >= 5) { "ユーザー名は5文字以上です" }
+    }
+
+    fun changeUserName(other: String) {
+        this.name = other
     }
 }
