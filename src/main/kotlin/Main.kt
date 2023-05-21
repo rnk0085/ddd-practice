f3f6b6f48796fe1087151fd09f7b05378aed6b98
rnk0085 <rinawkmbi4k8@gmail.com>
2023-05-21 18:13:43 +0900

    値オブジェクトとエンティティ

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,13 @@
-import domain.BadUserName
-import domain.User
-import domain.UserId
-import domain.UserName
+import domain.*
 
 fun main() {
-    check2()
+    check4()
+    check5()
 
-    check3()
+//    check2()
+//    check3()
+//
+//    check1()
 }
 
 /**
@@ -47,4 +48,48 @@ private fun check3() {
 
     rnk0085.changeUserName("takenoko")
     println("rnk0085 = $rnk0085")
+}
+
+/**
+ * 値オブジェクトのはず…？
+ */
+private fun check4() {
+    val rnk0085User = User(
+        id = UserId(0),
+        name = UserName("rnk0085"),
+    )
+    println("rnk0085User = $rnk0085User")
+
+    val takenokoUser = rnk0085User.changeUserName(
+        other = UserName("takenoko")
+    )
+    println("takenokoUser = $takenokoUser")
+
+    // 値オブジェクトの場合は全ての属性によって比較される（等価性）
+    println("rnk0085User == takenokoUser -> ${rnk0085User == takenokoUser}") // 期待: false
+}
+
+/**
+ * エンティティにしたかった
+ */
+private fun check5() {
+    val rnk0085 = UserEntity(
+        id = UserId(0),
+        name = UserName("rnk0085"),
+    )
+    println("rnk0085 = $rnk0085")
+
+    rnk0085.changeUserName(
+        other = UserName("takenoko")
+    )
+    println("rnk0085 = $rnk0085")
+
+    val temp = UserEntity(
+        id = UserId(0),
+        name = UserName("rnk0085"),
+    )
+    println("temp = $temp")
+
+    // エンティティは同一性を表す識別子(id)だけが比較の対象
+    println("rnk0085 == temp -> ${rnk0085 == temp}") // 期待: true
 }
--- src/main/kotlin/domain/User.kt
+++ src/main/kotlin/domain/User.kt
@@ -6,7 +6,28 @@ package domain
 data class User(
     val id: UserId,
     val name: UserName,
-)
+) {
+    fun changeUserName(other: UserName) : User {
+        return this.copy(name = other)
+    }
+}
+
+/**
+ * エンティティ
+ *  ・属性を変化させたい場合は、ふるまいを通じて変更する
+ *  ・必要に応じて属性を可変にすることを許可されている
+ *
+ *  TODO: エンティティは同一性で判断するはずなので、その部分は作らないといけなさそう…？
+ */
+data class UserEntity(
+    val id: UserId,
+    var name: UserName, // 可変
+) {
+    fun changeUserName(other: UserName) {
+        this.name = other
+    }
+}
+
 
 /**
  * 好ましくない。
